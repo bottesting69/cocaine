50cff6335131c432034a32e34131301ba36d48d3
bottesting69 <59135049+bottesting69@users.noreply.github.com>
2020-12-25 20:17:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var Client = require("mpp-client");
 var MPP = new Client("wss://www.multiplayerpiano.com:443");
-MPP.setChannel (defaultRoom);
 var defaultRoom = "Cocaine";
+MPP.setChannel (defaultRoom);
 MPP.start();
